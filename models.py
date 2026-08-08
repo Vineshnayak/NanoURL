793a659bddfe7ba58c666e1c5e0c82d5b9ae3448
Vinesh nayak <vineshnayak.jatothu@gmail.com>
2026-08-08 23:30:36 +0530

    Retain changes to 10

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class URLMapping(Base):
     __tablename__ = "url_mappings"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    short_code = Column(String(5), unique=True, index=True, nullable=True)
+    short_code = Column(String(10), unique=True, index=True, nullable=True)
     original_url = Column(Text, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     expires_at = Column(DateTime(timezone=True), nullable=True)

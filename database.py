26a57170db0f8fa5ca83599942415c6b65bd719c
Vinesh nayak <vineshnayak.jatothu@gmail.com>
2026-08-12 08:49:30 +0530

    Sqlalchemy db url preserving to False

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./nanourl.db"
 
 # connect_args={"check_same_thread": False} is needed only for SQLite
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": True}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
